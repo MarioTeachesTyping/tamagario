9b085dfdc56f0b416b47e7886ff1963e93f305b8
Anthony Terry <terryant64@gmail.com>
2025-11-29 16:30:39 -0500

    data and settings button work

--- app/src/main/java/com/example/tamagario/MainActivity.java
+++ app/src/main/java/com/example/tamagario/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tamagario;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +18,25 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Button Navigation Code
+        ImageView dataBtn = findViewById(R.id.data_button);
+        ImageView settingsBtn = findViewById(R.id.settings_button);
+
+        dataBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DataActivity.class);
+            startActivity(intent);
+        });
+
+        settingsBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
         });
     }
 }
